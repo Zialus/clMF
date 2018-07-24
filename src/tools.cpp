cfc1f0afca55c65be28c9bc01bf3ef1cc07b2695
Raul Ferreira <raulmendesferreira@gmail.com>
2018-07-24 15:16:43 +0100

    Removed silly "silent failure" if.

--- src/tools.cpp
+++ src/tools.cpp
@@ -88,10 +88,6 @@ int convertToString(const char* filename, std::string& s) {
         size = fileSize = (size_t) f.tellg();
         f.seekg(0, std::fstream::beg);
         str = new char[size + 1];
-        if (!str) {
-            f.close();
-            return 0;
-        }
         f.read(str, fileSize);
         f.close();
         str[size] = '\0';

7ded42b9153f3354c0b58e843a5b1ab8fa27301e
Raul Ferreira <raulmendesferreira@gmail.com>
2018-09-16 15:00:08 +0100

    Better formatting.

--- src/tools.cpp
+++ src/tools.cpp
@@ -291,12 +291,10 @@ void load(const char* srcdir, smat_t& R, bool ifALS, bool with_weights) {
 void initial_col(mat_t& X, long k, long n) {
     X = mat_t(k, vec_t(n));
     srand(0L);
-    //srand48(0L);
     long i, j;
     for (i = 0; i < n; ++i) {
         for (j = 0; j < k; ++j) {
             X[j][i] = 0.1 * (float(rand()) / RAND_MAX) + 0.001;
-            //X[j][i] = 0.1*drand48();
         }
     }
 }

49c34e4b41f612f7086608eea5fa390d4ab0b1e5
Raul Ferreira <raulmendesferreira@gmail.com>
2018-09-18 16:43:41 +0100

    Fixed more sign mismatch warnings.

--- src/tools.cpp
+++ src/tools.cpp
@@ -145,7 +145,6 @@ cl_device_id* getDevice(cl_platform_id& platform, char* device_type) {
 }
 
 void print_all_the_info() {
-    int i, j;
     char* value;
     size_t valueSize;
     cl_uint platformCount;
@@ -157,7 +156,7 @@ void print_all_the_info() {
     cl_platform_id* platforms = (cl_platform_id*) malloc(sizeof(cl_platform_id) * platformCount);
     clGetPlatformIDs(platformCount, platforms, nullptr);
 
-    for (i = 0; i < platformCount; i++) {
+    for (unsigned i = 0; i < platformCount; i++) {
 
         // get all devices
         clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, nullptr, &deviceCount);
@@ -165,40 +164,40 @@ void print_all_the_info() {
         clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, deviceCount, devices, nullptr);
 
         // for each device print critical attributes
-        for (j = 0; j < deviceCount; j++) {
+        for (unsigned j = 0; j < deviceCount; j++) {
 
             // print device name
             clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, nullptr, &valueSize);
             value = (char*) malloc(valueSize);
             clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, nullptr);
-            printf("%d. Device: %s\n", j + 1, value);
+            printf("%u. Device: %s\n", j + 1, value);
             free(value);
 
             // print hardware device version
             clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, 0, nullptr, &valueSize);
             value = (char*) malloc(valueSize);
             clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, valueSize, value, nullptr);
-            printf(" %d.%d Hardware version: %s\n", j + 1, 1, value);
+            printf(" %u.%u Hardware version: %s\n", j + 1, 1, value);
             free(value);
 
             // print software driver version
             clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, 0, nullptr, &valueSize);
             value = (char*) malloc(valueSize);
             clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, valueSize, value, nullptr);
-            printf(" %d.%d Software version: %s\n", j + 1, 2, value);
+            printf(" %u.%u Software version: %s\n", j + 1, 2, value);
             free(value);
 
             // print c version supported by compiler for device
             clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, 0, nullptr, &valueSize);
             value = (char*) malloc(valueSize);
             clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, valueSize, value, nullptr);
-            printf(" %d.%d OpenCL C version: %s\n", j + 1, 3, value);
+            printf(" %u.%u OpenCL C version: %s\n", j + 1, 3, value);
             free(value);
 
             // print parallel compute units
             clGetDeviceInfo(devices[j], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(maxComputeUnits), &maxComputeUnits,
                             nullptr);
-            printf(" %d.%d Parallel compute units: %d\n", j + 1, 4, maxComputeUnits);
+            printf(" %u.%u Parallel compute units: %u\n", j + 1, 4, maxComputeUnits);
 
         }
 
@@ -210,7 +209,6 @@ void print_all_the_info() {
 }
 
 void print_all_the_platforms() {
-    int i, j;
     char* info;
     size_t infoSize;
     cl_uint platformCount;
@@ -229,11 +227,11 @@ void print_all_the_platforms() {
     clGetPlatformIDs(platformCount, platforms, nullptr);
 
     // for each platform print all attributes
-    for (i = 0; i < platformCount; i++) {
+    for (unsigned i = 0; i < platformCount; i++) {
 
-        printf("%d. Platform \n", i + 1);
+        printf("%u. Platform \n", i + 1);
 
-        for (j = 0; j < attributeCount; j++) {
+        for (unsigned j = 0; j < attributeCount; j++) {
 
             // get platform attribute value size
             clGetPlatformInfo(platforms[i], attributeTypes[j], 0, nullptr, &infoSize);
@@ -242,7 +240,7 @@ void print_all_the_platforms() {
             // get platform attribute value
             clGetPlatformInfo(platforms[i], attributeTypes[j], infoSize, info, nullptr);
 
-            printf(" %d.%d %-11s: %s\n", i + 1, j + 1, attributeNames[j], info);
+            printf(" %u.%u %-11s: %s\n", i + 1, j + 1, attributeNames[j], info);
             free(info);
 
         }

f23fe972996e5d3ae80c38ec3e255c2c7f9e7cbc
Raul Ferreira <raulmendesferreira@gmail.com>
2018-09-25 16:15:49 +0100

    Lot's of improvements to appveyor config.

---
+++ scripts/amdappsdk_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -exu
+
+wget -q https://github.com/Microsoft/LightGBM/releases/download/v2.0.12/AMD-APP-SDKInstaller-v3.0.130.136-GA-linux64.tar.bz2
+tar -xjf AMD-APP-SDK*.tar.bz2
+
+export AMDAPPSDKROOT=$HOME/AMDAPPSDK
+export OPENCL_VENDOR_PATH=${AMDAPPSDKROOT}/etc/OpenCL/vendors
+export LD_LIBRARY_PATH=${AMDAPPSDKROOT}/lib/x86_64:$LD_LIBRARY_PATH
+
+mkdir -p ${OPENCL_VENDOR_PATH}
+mkdir -p ${AMDAPPSDKROOT}
+
+sh AMD-APP-SDK*.sh --tar -xf -C ${AMDAPPSDKROOT}
+mv ${AMDAPPSDKROOT}/lib/x86_64/sdk/* ${AMDAPPSDKROOT}/lib/x86_64/
+echo libamdocl64.so > ${OPENCL_VENDOR_PATH}/amdocl64.icd
+
+set +exu

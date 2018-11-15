f7f351bf1e20f7311b4cf0f333bf6ec74b311d39
Raul Ferreira <raulmendesferreira@gmail.com>
2018-11-15 11:24:10 +0000

    Newer version of OpenCL driver for Linux.

--- scripts/opencl_install.sh
+++ scripts/opencl_install.sh
@@ -6,9 +6,9 @@ set -exu
 # Get Intel OpenCL Runtime
 ###########################
 
-# https://software.intel.com/en-us/articles/opencl-drivers#latest_CPU_runtime
-PACKAGE_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/12556/opencl_runtime_16.1.2_x64_rh_6.4.0.37.tgz
-PACKAGE_NAME=opencl_runtime_16.1.2_x64_rh_6.4.0.37
+# https://software.intel.com/en-us/articles/opencl-drivers#cpu-section
+PACKAGE_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/13793/l_opencl_p_18.1.0.013.tgz
+PACKAGE_NAME=l_opencl_p_18.1.0.013
 
 wget -q ${PACKAGE_URL} -O /tmp/opencl_runtime.tgz
 tar -xzf /tmp/opencl_runtime.tgz -C /tmp

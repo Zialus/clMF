db0d7ff87a33494d2fa590e98dfe1a5b9e5303e8
Raul Ferreira <raulmendesferreira@gmail.com>
2018-12-22 01:52:17 +0000

    Reorder some parameters.

--- src/pmf.h
+++ src/pmf.h
@@ -4,18 +4,22 @@
 class parameter {
 public:
     int threads = 16;
-    int do_ref = 0; // compare opencl results to reference results
-    int do_predict = 0;
-    unsigned k = 10;
-    int maxiter = 5;
-    float lambda = 0.05f;
     int nBlocks = 8192;
     int nThreadsPerBlock = 32;
+
+    int do_ref = 0; // compare opencl results to reference results
+    int do_predict = 0;
+
+    int maxiter = 5;
+    unsigned k = 10;
+    float lambda = 0.05f;
+
     int platform_id = 0;
     int verbose = 0;
+
     char device_type[4] = {'g', 'p', 'u', '\0'};
-    char opencl_filename[1024] = "../kcode/ALS.cl";
     char src_dir[1024] = "../data/simple";
+    char opencl_filename[1024] = "../kcode/ALS.cl";
 };
 
 #endif

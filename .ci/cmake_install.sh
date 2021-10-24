298b824350cf55d89da8046298c2820c62ead616
Raul Ferreira <raulmendesferreira@gmail.com>
2021-10-24 16:27:04 +0100

    Update CMake installer

--- .ci/cmake_install.sh
+++ .ci/cmake_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/Kitware/CMake/releases/download/v3.13.2/cmake-3.13.2-Linux-x86_64.sh
-sudo sh cmake-3.13.2-Linux-x86_64.sh --prefix="$HOME"/.local/ --exclude-subdir
+export CMAKE_VERSION=3.21.3
+
+wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-Linux-x86_64.sh
+sudo sh cmake-${CMAKE_VERSION}-Linux-x86_64.sh --prefix="$HOME"/.local/ --exclude-subdir
